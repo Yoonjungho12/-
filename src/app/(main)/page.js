f90677950139974ab4c0a65fc3576023848ca400
leeminje0411 <mj12270411@gmail.com>
2025-03-07 02:13:29 +0900

    팜하니sse

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Mainone from "../components/main/main1";
+import RecommendedShops from "../components/main/recommendedShop";
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <Image
+    <>
+  
+     
+      <div className="container mx-auto">
+       
+<Image
         src="https://cdn.vipgunma.com/assets/banner/1675135222banner2.jpg"
         alt="Banner"
         width={1250}
@@ -15,8 +21,8 @@ export default function Home() {
       />
       <Mainone/>
       <RecommendedShops/>
-    </div>
+      </div>
+    </>
   );
 }
-
 
