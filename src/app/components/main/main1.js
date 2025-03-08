64721e25a983488adb29f991992cb8e52d542d16
leeminje0411 <mj12270411@gmail.com>
2025-03-08 21:52:48 +0900

    출근2부

--- src/app/components/main/main1.js
+++ src/app/components/main/main1.js
@@ -321,7 +321,7 @@ export default function PopularShops() {
                     width={400}
                     height={300}
                     style={{ objectFit: "cover" }}
-                      quality={1}
+                      quality={30}
                 //    sizes="(max-width: 768px) 360px, 400px"
                   priority
                   />
