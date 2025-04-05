07e38e13b3e82b1c3e871cb280df27cb5df417c1
leeminje0411 <mj12270411@gmail.com>
2025-04-06 07:18:39 +0900

    style: 네비게이션 바 스타일 수정

--- src/app/lib/supabaseF.js
+++ src/app/lib/supabaseF.js
@@ -1,4 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,  // default는 true이지만 명시해도 좋음
+      storage: typeof window !== 'undefined' ? localStorage : undefined
+    }
+  }
 );
